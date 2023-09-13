fde0d57ecd169a086e7b57e70f06e118179f57ae
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-13 11:35:42 +0700

    feat: create auth controller

---
+++ controllers/AuthController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+import Authentication from "../utils/Authentication";
+
+class AuthController {
+	register = async (req: Request, res: Response) => {
+		const { name, email, password } = req.body;
+		const hashPassword = await Authentication.passwordHash(password);
+
+		const user = await User.findOne({
+			email,
+		});
+
+		if (!user) {
+			const newUser = new User({
+				name,
+				email,
+				password: hashPassword,
+			});
+
+			await newUser.save();
+
+			return res.status(200).send({
+				status: "success",
+				message: "user has been successfully registered",
+			});
+		} else {
+			return res.status(400).json({
+				message: "User email already exist!",
+			});
+		}
+	};
+	login = async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const user = await User.findOne({
+			email,
+		});
+
+		if (user) {
+			const isMatchPassword = await Authentication.comparePassword(
+				password,
+				user.password
+			);
+
+			if (isMatchPassword) {
+				const token = Authentication.generateToken({
+					id: user.id,
+				});
+
+				return res.status(200).send({
+					status: "succss",
+					message: "User has been successfully login",
+					token,
+				});
+			}
+		} else {
+			return res.status(404).send({
+				status: "failed",
+				message: "User Email not found! or Wrong password",
+			});
+		}
+	};
+}
+
+export default new AuthController();
