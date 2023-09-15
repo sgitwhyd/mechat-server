14d2dd765240de6cb3644a64659527c45fbdf461
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-16 00:27:10 +0700

    refactor: adding erorr handler if room id undefined

--- controllers/ChatController.ts
+++ controllers/ChatController.ts
@@ -3,22 +3,29 @@ import Chat from "../models/ChatModel";
 
 class ChatController {
 	index = async (req: Request, res: Response) => {
-		const { room_id } = req.body;
+		const { room_id } = req.params;
 
-		const chats = await Chat.find({
-			roomId: room_id,
-		}).populate("user_id");
+		try {
+			const chats = await Chat.find({
+				roomId: room_id,
+			}).populate("user_id");
 
-		if (chats) {
-			return res.status(200).json({
-				status: "success",
-				message: "Chat showed",
-				data: chats,
-			});
-		} else {
+			if (chats) {
+				return res.status(200).json({
+					status: "success",
+					message: "Chat showed",
+					data: chats,
+				});
+			} else {
+				return res.status(404).json({
+					status: "failed",
+					message: "Chat Not Found",
+				});
+			}
+		} catch (error) {
 			return res.status(404).json({
 				status: "failed",
-				message: "Chat Not Found",
+				message: "Room Not Found",
 			});
 		}
 	};
